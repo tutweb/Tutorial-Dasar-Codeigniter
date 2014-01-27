7d7d6d91b306d0154663c12663c0a808c183d895
AH <s.adrianh@gmail.com>
2014-01-27 12:08:23 +0700

    + file “Tutorial Dasar Penggunaan CodeIgniter”

---
+++ CodeIgniter/application/views/say_greetings.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<title>Belajar CodeIgniter</title>
+	</head>
+	<body>
+		<a href="http://www.adrianhartanto.com">
+			<h1>
+				Greetings from CodeIgniter, <br /> by Adrian Hartanto
+			</h1>
+		</a>
+	</body>
+</html>
